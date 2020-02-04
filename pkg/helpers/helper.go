c32b69441fefe44110a81e62a76cef326607364e
vadiminshakov <vadiminshakov@gmail.com>
2020-02-04 11:04:48 +0300

    view for identity removal added

--- pkg/helpers/helper.go
+++ pkg/helpers/helper.go
@@ -11,3 +11,14 @@ func ShowRevoked(RevocationResponse *msp.RevocationResponse) {
 		fmt.Printf("%d. Serial: %s, AKI: %s\n", index, revokedCert.Serial, revokedCert.AKI)
 	}
 }
+
+func ShowRemoved(identityResponse *msp.IdentityResponse) {
+	fmt.Printf("Removed identity:\nID: %s\nAffiliation: %s\nAttributes: %v\nType: %s\nMaxEnrollments: %d\nSecret: %s\nCAName: %s\n",
+		identityResponse.ID,
+		identityResponse.Affiliation,
+		identityResponse.Attributes,
+		identityResponse.Type,
+		identityResponse.MaxEnrollments,
+		identityResponse.Secret,
+		identityResponse.CAName)
+}
